420a50b5a071a98283c5fac16368ff46eec5dbbf
Jaggu <Jagadeesh.r.chowdekar@gmail.com>
2024-10-06 16:41:18 +0530

    login 1st phase

---
+++ src/main/java/demo/LoginServlet.java
@@ -0,0 +1,20 @@
+package demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//Add Login Logic Here Vaheeb Please...!
+		
+		
+	}
+
+}
